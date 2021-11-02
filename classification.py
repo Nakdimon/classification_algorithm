56d00bf5455ad0db445d2534de5d4713082b352f
Nakdimon <gjherrera.i@gmail.com>
2021-11-02 17:50:26 +0800

    Testing probability calculation with test dataset

--- classification.py
+++ classification.py
@@ -1,5 +1,7 @@
 # Split the dataset by class values, returns a dictionary
 from math import sqrt
+from math import pi
+from math import exp
 
 
 def separate_by_class(dataset):
@@ -36,6 +38,36 @@ def summarize_dataset(dataset):
     return summaries
 
 
+# Split dataset by class then calculate statistics for each row
+def summarize_by_class(dataset):
+    separated = separate_by_class(dataset)
+    summaries = dict()
+    for class_value, rows in separated.items():
+        summaries[class_value] = summarize_dataset(rows)
+    return summaries
+
+# Calculate the Gaussian probability distribution function for x
+
+
+def calculate_probability(x, mean, stdev):
+    exponent = exp(-((x-mean)**2 / (2 * stdev**2)))
+    return (1 / (sqrt(2 * pi) * stdev)) * exponent
+
+
+# Calculate the probabilities of predicting each class for a given row
+def calculate_class_probabilities(summaries, row):
+    total_rows = sum([summaries[label][0][2] for label in summaries])
+    probabilities = dict()
+    for class_value, class_summaries in summaries.items():
+        probabilities[class_value] = summaries[class_value][0][2] / \
+            float(total_rows)
+        for i in range(len(class_summaries)):
+            mean, stdev, count = class_summaries[i]
+            probabilities[class_value] *= calculate_probability(
+                row[i], mean, stdev)
+    return probabilities
+
+
 dataset = [[3.393533211, 2.331273381, 0],
            [3.110073483, 1.781539638, 0],
            [1.343808831, 3.368360954, 0],
@@ -47,5 +79,6 @@ dataset = [[3.393533211, 2.331273381, 0],
            [7.792783481, 3.424088941, 1],
            [7.939820817, 0.791637231, 1]]
 
-summary = summarize_dataset(dataset)
-print(summary)
+summaries = summarize_by_class(dataset)
+probabilities = calculate_class_probabilities(summaries, dataset[0])
+print(probabilities)
